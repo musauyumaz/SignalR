095af1b1aad88f449dbf93978a49ab4336ac4098
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-12 17:13:30 +0300

    feat: Add SignalR client script and integrate with Home/Index view

---
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    const connection = new signalR.HubConnectionBuilder().withUrl("/exampleHub").configureLogging(signalR.LogLevel.Information).build();
+    
+    
+    function start(){
+        connection.start().then(() => console.log("Hub ile Bağlantı Kuruldu"));
+    }
+    try {
+        start();
+    }catch (e) {
+        setTimeout(() => start(),5000)
+    }
+    
+    connection.on("ReceiveMessageForAllClient", (message) => {
+        console.log("Gelen Mesaj : " + message);
+    })
+    
+    $("#btnSendMessageAllClient").click(function () {
+        const message = "hello world";
+        connection.invoke("BroadCastMessageToAllClient", message).catch(err => console.error("hata", err));
+    })
+});
