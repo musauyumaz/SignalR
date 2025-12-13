d1bfd4b5fda13e806e83c81ab467487e7af8b550
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-13 12:10:25 +0300

    feat: Added buttons for joining and leaving groups A and B in the Index.cshtml view.
    * Implemented JavaScript functions to manage group membership and send messages to group clients.
    * This improves the codebase by enabling real-time group communication and user interaction with groups.
    * Introduced a dynamic list display for current group memberships, enhancing user experience.
    * Added error handling for group membership actions to ensure better feedback for users.

--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/wwwroot/js/signalr-client.js
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/wwwroot/js/signalr-client.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
     const connection = new signalR.HubConnectionBuilder().withUrl("/exampleTypeSafeHub").configureLogging(signalR.LogLevel.Information).build();
+
+    let currentGroupList = []
+    function refreshGroupList(){
+        $("#groupList").empty();
+        currentGroupList.forEach(group => {
+            $("#groupList").append(`<p>${group}</p>`)
+        })
+    }
     
+    $("#btnGroupAAdd").click(function () {
+        
+        if (currentGroupList.includes("GroupA")) return alert("Zaten Bu Grupta Bulunuyorsunuz")
+        
+        connection.invoke("AddUserToGroupAsync","GroupA").then(() => {
+            currentGroupList.push("GroupA");
+            refreshGroupList();
+        })
+    }) 
+    $("#btnGroupARemove").click(function () {
+        if (!currentGroupList.includes("GroupA")) return alert("Zaten Bu Grupta Bulunmuyorsun")
+        connection.invoke("RemoveUserToGroupAsync","GroupA").then(() => {
+            currentGroupList = currentGroupList.filter(group => group !== "GroupA");
+            refreshGroupList();
+        })
+    })
+
+    $("#btnGroupBAdd").click(function () {
+        if (currentGroupList.includes("GroupB")) return alert("Zaten Bu Grupta Bulunuyorsunuz")
+        connection.invoke("AddUserToGroupAsync","GroupB").then(() => {
+            currentGroupList.push("GroupB");
+            refreshGroupList();
+        })
+    })
+    $("#btnGroupBRemove").click(function () {
+        if (!currentGroupList.includes("GroupB")) return alert("Zaten Bu Grupta Bulunmuyorsun")
+        connection.invoke("RemoveUserToGroupAsync","GroupB").then(() => {
+            currentGroupList = currentGroupList.filter(group => group !== "GroupB");
+            refreshGroupList();
+        })
+    })
     
     function start(){
         connection.start().then(() => {
@@ -34,6 +73,11 @@ $(document).ready(function () {
         $("#divIndividualClientMessages").append("<li>" + message + "</li>");
     });
     
+    connection.on("ReceiveMessageForGroupClients", (message) => {
+        console.log("Group Gelen Mesaj : " + message);
+        $("#divGroupClientMessages").append("<li>" + message + "</li>");
+    });
+    
     let spanClientCount = $("#spanConnectedClientCount");
     connection.on("ReceiveConnectedClientCountAllClient", (count) => {
         spanClientCount.text(count);
@@ -57,5 +101,15 @@ $(document).ready(function () {
         const message = "Hello world individual client";
         const connectionId = $("#txtConnectionId").val();
         connection.invoke("BroadCastMessageToIndividualClient",connectionId, message).catch(err => console.error("hata", err));
+    })
+    $("#btnGroupASendMessage").click(function () {
+        const message = "Group A Message";
+        const connectionId = $("#txtConnectionId").val();
+        connection.invoke("BroadcastMessageToGroupClients","GroupA", message).catch(err => console.error("hata", err));
+    });
+    $("#btnGroupBSendMessage").click(function () {
+        const message = "Group B Message";
+        const connectionId = $("#txtConnectionId").val();
+        connection.invoke("BroadcastMessageToGroupClients","GroupB", message).catch(err => console.error("hata", err));
     })
 });
