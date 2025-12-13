5d33c0a61cf6eee702e40ffd970d98b189db0afb
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-13 19:26:47 +0300

    feat(console-app): add SignalR console application for product messaging
    * A new class `Product` was created to represent product data with properties for Id, Name, and Price.
    * The `Program.cs` file was added to establish a SignalR connection and listen for messages of type `Product`.
    * This implementation allows the console application to receive real-time updates about products, enhancing interactivity.
    * The project file now includes a reference to the `Microsoft.AspNetCore.SignalR.Client` package, enabling SignalR functionality.
    * The solution file was updated to include the new console application project, ensuring it integrates with existing projects.

---
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.SignalR.Client;
+using SignalR.ConsoleApp;
+
+Console.WriteLine("SignalR Console Client");
+
+var connection = new HubConnectionBuilder().WithUrl("https://localhost:7133/exampleTypeSafeHub").Build();
+
+connection.StartAsync().ContinueWith(result => Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Failed to connect"));
+
+connection.On<Product>("ReceiveTypedMessageForAllClient", (product) => Console.WriteLine($"Received Message: {product.Id} - {product.Name} : {product.Price}"));
+
+Console.ReadLine();
