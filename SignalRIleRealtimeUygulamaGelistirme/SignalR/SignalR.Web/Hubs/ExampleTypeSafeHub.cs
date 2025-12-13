64076c0547621ed0a9cc06d57f0b0c86a853571b
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-13 07:33:02 +0300

    feat(hub): Add example type-safe hub

--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/Hubs/ExampleTypeSafeHub.cs
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/Hubs/ExampleTypeSafeHub.cs
@@ -4,22 +4,23 @@ namespace SignalR.Web.Hubs;
 
 public class ExampleTypeSafeHub : Hub<IExampleTypeSafeHub>
 {
-    private static int connectedClientCount = 0;
+    private static int _connectedClientCount = 0;
     public async Task BroadCastMessageToAllClient(string message) => await Clients.All.ReceiveMessageForAllClient(message);
     public async Task BroadCastMessageToCallerClient(string message) => await Clients.Caller.ReceiveMessageForCallerClient(message);
     public async Task BroadCastMessageToOthersClient(string message) => await Clients.Others.ReceiveMessageForOthersClient(message);
+    public async Task BroadCastMessageToIndividualClient(string connectionId, string message) => await Clients.Client(connectionId).ReceiveMessageForIndividualClient(message);
 
-    public override async Task OnConnectedAsync()
+    public override async Task OnConnectedAsync() 
     {
-        connectedClientCount++;
-        await Clients.All.ReceiveConnectedClientCountAllClient(connectedClientCount);
+        _connectedClientCount++;
+        await Clients.All.ReceiveConnectedClientCountAllClient(_connectedClientCount);
         await base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        connectedClientCount--;
-        await Clients.All.ReceiveConnectedClientCountAllClient(connectedClientCount);
+        _connectedClientCount--;
+        await Clients.All.ReceiveConnectedClientCountAllClient(_connectedClientCount);
         await base.OnDisconnectedAsync(exception);
     }
 }
--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/Hubs/IExampleTypeSafeHub.cs
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/Hubs/IExampleTypeSafeHub.cs
@@ -5,5 +5,6 @@ public interface IExampleTypeSafeHub
     Task ReceiveMessageForAllClient(string message);
     Task ReceiveMessageForCallerClient(string message);
     Task ReceiveMessageForOthersClient(string message);
+    Task ReceiveMessageForIndividualClient(string message);
     Task ReceiveConnectedClientCountAllClient(int clientCount);
 }
