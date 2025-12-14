e5674bff9ae401e53589877dc8a98fdfb1c6539a
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-14 12:23:44 +0300

    feat(stream): add product streaming functionality
    * A new button for sending product data has been added to the Stream.cshtml view.
    * The JavaScript now includes a method to handle product streaming and display product details in the stream box.
    * This enhances the user experience by allowing real-time updates of product information alongside existing name streaming.
    * The new product list is hardcoded for demonstration purposes, providing a clear structure for future dynamic data integration.

--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/wwwroot/js/signalr-client-stream.js
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.Web/wwwroot/js/signalr-client-stream.js
@@ -22,6 +22,10 @@ $(document).ready(function () {
     connection.on("ReceiveMessageAsStreamForAllClient", (name) => {
         $("#streamBox").append(`<li>${name}</li>`)
     })
+    connection.on("ReceiveProductAsStreamForAllClient", (product) => {
+        $("#streamBox").append(`<li>${product.id} - ${product.name} - ${product.price}</li>`)
+    })
+    
     
     $("#btnFromClientToHub").click(function () {
         const names = $("#txtStream").val();
@@ -33,6 +37,31 @@ $(document).ready(function () {
         
         nameAsChunk.forEach(name => {
             subject.next(name);
+        });
+        
+        subject.complete();
+    });
+    
+    $("#btnProductFromClientToHub").click(function () {
+        var productList = [
+            {id: 1, name: "pen 1", price: 200},
+            {id: 2, name: "pen 2", price: 300},
+            {id: 3, name: "pen 3", price: 400},
+            {id: 4, name: "pen 4", price: 500},
+            {id: 5, name: "pen 5", price: 600},
+            {id: 6, name: "pen 6", price: 700},
+            {id: 7, name: "pen 7", price: 800},
+            {id: 8, name: "pen 8", price: 900},
+            {id: 9, name: "pen 9", price: 1000},
+            {id: 10, name: "pen 10", price: 1100}
+        ]
+        
+        const subject = new signalR.Subject();
+        
+        connection.send("BroadcastStreamProductToAllClient", subject).catch(err => console.error("hata", err));
+
+        productList.forEach(product => {
+            subject.next(product);
         });
         
         subject.complete();
