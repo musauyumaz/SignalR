b6dcc8f55ee4f53b400ed0dd721410c2b7095a56
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-19 17:40:39 +0300

    feat(HomeController): add product creation and retrieval logic
    * Implemented logic to create default products for a user if none exist
    * Updated ProductList action to fetch and display products associated with the user

--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/Controllers/HomeController.cs
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalR.SampleProject.Models.Contexts;
+using SignalR.SampleProject.Models.Entities;
 using SignalR.SampleProject.Models.ViewModels;
 
 namespace SignalR.SampleProject.Controllers;
 
-public class HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) : Controller
+public class HomeController(
+    UserManager<IdentityUser> userManager,
+    SignInManager<IdentityUser> signInManager,
+    AppDbContext dbContext) : Controller
 {
     public IActionResult Index()
     {
@@ -21,6 +27,7 @@ public class HomeController(UserManager<IdentityUser> userManager, SignInManager
     {
         return View();
     }
+
     [HttpPost]
     public async Task<IActionResult> SignUp(SignUpViewModel model)
     {
@@ -46,22 +53,48 @@ public class HomeController(UserManager<IdentityUser> userManager, SignInManager
     {
         return View();
     }
+
     [HttpPost]
     public async Task<IActionResult> SignIn(SignInViewModel model)
     {
         if (!ModelState.IsValid) return View(model);
-        
+
         var hasUser = await userManager.FindByEmailAsync(model.Email);
-        if (hasUser == null) ModelState.AddModelError(string.Empty, "Email or Password is incorrect") ;
-        
+        if (hasUser == null) ModelState.AddModelError(string.Empty, "Email or Password is incorrect");
+
         var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
-        if (!result.Succeeded) ModelState.AddModelError(string.Empty, "Email or Password is incorrect") ;
-        
+        if (!result.Succeeded) ModelState.AddModelError(string.Empty, "Email or Password is incorrect");
+
         return RedirectToAction(nameof(Index));
     }
 
-    public IActionResult ProductList()
+    public async Task<IActionResult> ProductList()
     {
-        return View();
+        var user = await userManager.Users.FirstOrDefaultAsync(x => x.Id == "fa34618b-17ce-4824-b40e-a3062a943fa0");
+
+        if (!dbContext.Products.Any(x => x.UserId == user.Id))
+        {
+            var product1 = new Product()
+                { Name = "Pen 1", Description = "Description 1", Price = 100, UserId = user.Id };
+            var product2 = new Product()
+                { Name = "Pen 2", Description = "Description 2", Price = 200, UserId = user.Id };
+            var product3 = new Product()
+                { Name = "Pen 3", Description = "Description 3", Price = 300, UserId = user.Id };
+            var product4 = new Product()
+                { Name = "Pen 4", Description = "Description 4", Price = 400, UserId = user.Id };
+            var product5 = new Product()
+                { Name = "Pen 5", Description = "Description 5", Price = 500, UserId = user.Id };
+
+            await dbContext.Products.AddAsync(product1);
+            await dbContext.Products.AddAsync(product2);
+            await dbContext.Products.AddAsync(product3);
+            await dbContext.Products.AddAsync(product4);
+            await dbContext.Products.AddAsync(product5);
+            await dbContext.SaveChangesAsync();
+        }
+
+
+        var products = await dbContext.Products.Where(x => x.UserId == user.Id).ToListAsync();
+        return View(products);
     }
 }
--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/Models/Contexts/AppDbContext.cs
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/Models/Contexts/AppDbContext.cs
@@ -8,4 +8,13 @@ namespace SignalR.SampleProject.Models.Contexts;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<IdentityUser, IdentityRole, string>(options)
 {
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .HasOne(p => p.IdentityUser)
+            .WithMany()
+            .HasForeignKey(p => p.UserId);
+        base.OnModelCreating(builder);
+    }
 }
