8a41d9ed0ec1f99cea460f96125f6b47fabae8b9
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-20 08:12:24 +0300

    feat(signalr/ui): integrate SignalR for real-time Excel notifications
    * Added SignalR script to `_Layout.cshtml`.
    * Set up SignalR connection in `site.js` for alerting file download readiness.
    * Enhanced error handling in `ProductList.cshtml` to alert non-members.

--- SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/wwwroot/js/site.js
+++ SignalRIleRealtimeUygulamaGelistirme/SignalR/SignalR.SampleProject/wwwroot/js/site.js
@@ -1,2 +1,14 @@
 ﻿
 let toastTimeOut;
+$(document).ready(function () {
+    const connection = new window.signalR.HubConnectionBuilder().withUrl("/hub").build();
+    
+    connection.start().then(() => {
+        console.log("Bağlantı sağlandı.")});
+    
+    connection.on("AlertCompleteFile", (downloadPath) => {
+        clearTimeout(toastTimeOut);
+        $(".toast-body").html(`<p>Excel oluşturma işlemi tamamlanmıştır. Aşağıdaki link ile excel dosyasını indirebilirsiniz.<p> <a href="${downloadPath}">indir</a>`);
+        $("#liveToast").show();
+    });
+})
