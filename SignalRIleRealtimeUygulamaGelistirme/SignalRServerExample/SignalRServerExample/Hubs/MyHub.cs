1cd0544758c455ce2a5e08ceef5cc7a35ba299a9
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-20 20:50:53 +0300

    feat(hub): add connection and disconnection handling
    * Implemented OnConnectedAsync to manage client connections
    * Added OnDisconnectedAsync to handle client disconnections

--- SignalRIleRealtimeUygulamaGelistirme/SignalRServerExample/SignalRServerExample/Hubs/MyHub.cs
+++ SignalRIleRealtimeUygulamaGelistirme/SignalRServerExample/SignalRServerExample/Hubs/MyHub.cs
@@ -4,8 +4,27 @@ namespace SignalRServerExample.Hubs;
 
 public class MyHub : Hub
 {
+    private static List<string> clients = new();
     public async Task SendMessageAsync(string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", message);
     }
+
+    public override async Task OnConnectedAsync()
+    {
+        clients.Add(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", clients);
+        await Clients.All.SendAsync("UserJoined", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        clients.Remove(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", clients);
+        await Clients.All.SendAsync("UserLeft", Context.ConnectionId);
+    }
 }
+#region BaglantiOlayları
+//Bağlantı olayları signalR uygulamalarında loglama için oldukça elverişli fonksiyonlardır.
+//ConnectionId : Hub'a bağlantı gerçekleştiren client'lara sistem tarafından verilen unique/tekil bir değerdir. Amacı client'ları birbirlerinden ayırmaktır.
+#endregion
